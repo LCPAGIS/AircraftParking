364bbb6e9236d08900963636c370608588f190d5
LCPAGIS <65737238+LCPAGIS@users.noreply.github.com>
2020-06-25 11:48:19 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,356 @@
+params=
+
+{
+
+"aircraftList":["A321-200","A330-200","A330-300","A340-300","A350-900","A380-800","B737_900W_P","B737-800","B747-400","B747-800","B757-200","B757-300","B767-200","B767-300","B767-400","B777-200","B777-300ER","B787-800","B787-900","CRJ 900","Embraer 145"],
+
+"renderer":{
+    "type": "unique-value",
+    "defaultSymbol": {
+        "type": "simple-fill"
+    },
+    "field": "Aircraft_Type",
+    "uniqueValueInfos": [
+        {
+            "value": "A321-200",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    65,
+                    118,
+                    41
+                ]
+            }
+        },
+        {
+            "value": "A330-200",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    116,
+                    155,
+                    48
+                ]
+            }
+        },
+        {
+            "value": "A330-300",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    49,
+                    176,
+                    94
+                ]
+            }
+        },
+        {
+            "value": "A340-300",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    239,
+                    28,
+                    205
+                ]
+            }
+        },
+        {
+            "value": "A350-900",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    223,
+                    45,
+                    176
+                ]
+            }
+        },
+        {
+            "value": "A380-800",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    78,
+                    205,
+                    143
+                ]
+            }
+        },
+        {
+            "value": "B737_900W_P",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    175,
+                    241,
+                    4
+                ]
+            }
+        },
+        {
+            "value": "B737-800",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    219,
+                    27,
+                    195
+                ]
+            }
+        },
+        {
+            "value": "B747-400",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    115,
+                    60,
+                    138
+                ]
+            }
+        },
+        {
+            "value": "B747-800",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    40,
+                    234,
+                    29
+                ]
+            }
+        },
+        {
+            "value": "B757-200",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    203,
+                    237,
+                    47
+                ]
+            }
+        },
+        {
+            "value": "B757-300",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    33,
+                    95,
+                    242
+                ]
+            }
+        },
+        {
+            "value": "B767-200",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    85,
+                    179,
+                    179
+                ]
+            }
+        },
+        {
+            "value": "B767-300",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    237,
+                    230,
+                    94
+                ]
+            }
+        },
+        {
+            "value": "B767-400",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    125,
+                    148,
+                    92
+                ]
+            }
+        },
+        {
+            "value": "B777-200",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    173,
+                    40,
+                    98
+                ]
+            }
+        },
+        {
+            "value": "B777-300ER",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    163,
+                    53,
+                    152
+                ]
+            }
+        },
+        {
+            "value": "B787-800",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    179,
+                    155,
+                    209
+                ]
+            }
+        },
+        {
+            "value": "B787-900",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    5,
+                    76,
+                    224
+                ]
+            }
+        },
+        {
+            "value": "CRJ 900",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    142,
+                    99,
+                    172
+                ]
+            }
+        },
+        {
+            "value": "Embraer 145",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    12,
+                    115,
+                    246
+                ]
+            }
+        }
+    ]
+},
+
+"aircraftLayerURL":"https://services5.arcgis.com/1i4u4qbghswhTPsM/arcgis/rest/services/Aircraft/FeatureServer/0",
+
+"fieldConfig":[
+    {
+        "name": "Aircraft_Type",
+        "label": "Aircraft Type"
+    },
+    {
+        "name": "Manufacturer",
+        "label": "Manufacturer"
+    },
+    {
+        "name": "Wingspan",
+        "label": "Wingspan"
+    },
+    {
+        "name": "Gross_Weight",
+        "label": "Empty_Weight"
+    },
+    {
+        "name": "Tail_Number",
+        "label": "Tail_Number"
+    },
+    {
+        "name": "Carrier",
+        "label": "Carrier"
+    },
+    {
+        "name": "MBG_Orientation",
+        "label": "MBG_Orientation"
+    },
+    {
+        "name": "Date_In",
+        "label": "Date In"
+    },
+    {
+        "name": "Date_Out",
+        "label": "Date Out"
+    },
+    {
+        "name": "Date_Fueled",
+        "label": "Date Fueled"
+    },
+    {
+        "name": "FuselageLength",
+        "label": "Fuselage Length"
+    },
+    {
+        "name": "Status",
+        "label": "Status"
+    }
+]
+
+},
+
+"aircraftLayerURL":"https://services5.arcgis.com/1i4u4qbghswhTPsM/arcgis/rest/services/Aircraft/FeatureServer/0",
+
+"fieldConfig":[
+    {
+        "name": "Aircraft_Type",
+        "label": "Aircraft Type"
+    },
+    {
+        "name": "Manufacturer",
+        "label": "Manufacturer"
+    },
+    {
+        "name": "Wingspan",
+        "label": "Wingspan"
+    },
+    {
+        "name": "Gross_Weight",
+        "label": "Empty_Weight"
+    },
+    {
+        "name": "Tail_Number",
+        "label": "Tail_Number"
+    },
+    {
+        "name": "Carrier",
+        "label": "Carrier"
+    },
+    {
+        "name": "MBG_Orientation",
+        "label": "MBG_Orientation"
+    },
+    {
+        "name": "Date_In",
+        "label": "Date In"
+    },
+    {
+        "name": "Date_Out",
+        "label": "Date Out"
+    },
+    {
+        "name": "Date_Fueled",
+        "label": "Date Fueled"
+    },
+    {
+        "name": "FuselageLength",
+        "label": "Fuselage Length"
+    },
+    {
+        "name": "Status",
+        "label": "Status"
+    }
+]
+
+}
